8401ae70509bc58b5e5f240e1fbe29e10e394ea8
Michael Rosanelli <michaelrosanelli105@gmail.com>
2022-12-01 08:31:36 -0500

    add icon to application

--- src/main/java/com/example/assignment2/Main.java
+++ src/main/java/com/example/assignment2/Main.java
@@ -3,23 +3,23 @@ package com.example.assignment2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Locale;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("games-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("img/icon.png")));
         stage.setTitle("Fighting Games!");
         stage.setScene(scene);
         stage.show();
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         launch();
     }
 }
